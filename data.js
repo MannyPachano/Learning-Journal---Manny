b0faa74635a8b2f45f6dd6113690f9a091d4406d
MannyPachano <144030180+MannyPachano@users.noreply.github.com>
2023-10-12 12:50:05 +0800

    Update data.js
    removed 'scrimba'

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ export const blogArray = [
         image: "article-image-01.png",
         date: "JULY 23, 2022",
         title: "Blog one",
-        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        content: "I'm excited to start a new learning journey as a Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         
     },
     
@@ -11,7 +11,7 @@ export const blogArray = [
         image: "article-image-02.png",
         date: "JULY 24, 2022",
         title: "Blog two",
-        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        content: "I'm excited to start a new learning journey as a Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         
     },
     
@@ -19,7 +19,7 @@ export const blogArray = [
         image: "article-image-03.png",
         date: "JULY 25, 2022",
         title: "Blog three",
-        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        content: "I'm excited to start a new learning journey as a Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         
     },
     
@@ -27,7 +27,7 @@ export const blogArray = [
         image: "article-image-01.png",
         date: "JULY 26, 2022",
         title: "Blog four",
-        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        content: "I'm excited to start a new learning journey as a Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         
     },
     
@@ -35,7 +35,7 @@ export const blogArray = [
         image: "article-image-02.png",
         date: "JULY 27, 2022",
         title: "Blog five",
-        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        content: "I'm excited to start a new learning journey as a Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         
     },
     
@@ -43,7 +43,7 @@ export const blogArray = [
         image: "article-image-03.png",
         date: "JULY 28, 2022",
         title: "Blog six",
-        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        content: "I'm excited to start a new learning journey as a Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         
     },
     
@@ -51,7 +51,7 @@ export const blogArray = [
         image: "article-image-01.png",
         date: "JULY 29, 2022",
         title: "Blog seven",
-        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        content: "I'm excited to start a new learning journey as a Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         
     },
     
@@ -59,7 +59,7 @@ export const blogArray = [
         image: "article-image-02.png",
         date: "JULY 30, 2022",
         title: "Blog eight",
-        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        content: "I'm excited to start a new learning journey as a Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         
     },
     
@@ -67,7 +67,7 @@ export const blogArray = [
         image: "article-image-03.png",
         date: "JULY 31, 2022",
         title: "Blog nine",
-        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        content: "I'm excited to start a new learning journey as a Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         
     },
     
@@ -75,7 +75,7 @@ export const blogArray = [
         image: "article-image-01.png",
         date: "AUGUST 1, 2022",
         title: "Blog ten",
-        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        content: "I'm excited to start a new learning journey as a Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         
     },
     
